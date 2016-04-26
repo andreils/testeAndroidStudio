20aca95b7723d0018ee4537e525bbc23b47ad783
andrei.sbruzzi <andreiwz@gmail.com>
2016-04-26 18:41:51 -0300

    Main Activity 1

--- app/src/main/java/fcdl/org/br/background/MainActivity.java
+++ app/src/main/java/fcdl/org/br/background/MainActivity.java
@@ -2,6 +2,7 @@ package fcdl.org.br.background;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //teste
+        Log.d("Teste ", "teste");
     }
 }
